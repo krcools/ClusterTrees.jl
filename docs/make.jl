8c2850a25505d056702eed115b8abd44d7ab1670
Kristof Cools <krcools@gmail.com>
2019-09-25 13:49:47 +0200

    Added deploydocs etc

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,7 @@
 using Documenter, ClusterTrees
 # using DocumenterMarkdown
 makedocs(sitename="ClusterTrees Documentation", clean=false)
+
+deploydocs(
+    repo = "github.com/krcools/ClusterTrees.jl.git",
+)

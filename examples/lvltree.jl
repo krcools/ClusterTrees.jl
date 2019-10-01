edff1f69f2430595826e1bc10c7bbbf9d378d815
Kristof Cools <krcools@gmail.com>
2019-10-01 10:58:27 +0200

    Refactored lvltree routing

--- examples/lvltree.jl
+++ examples/lvltree.jl
@@ -8,6 +8,7 @@ const P = SVector{3,Float64}
 mesh = meshsphere(1.0, 0.15)
 # points = [rand(P) for i in 1:800]
 points = vertices(mesh)
+points = [cartesian(CompScienceMeshes.center(chart(mesh, c))) for c in cells(mesh)]
 
 # using DelimitedFiles
 # Q = readdlm("points.dlm", Float64)
@@ -23,7 +24,8 @@ root_sector = 0
 root_sfc_state = 1
 for i in 1:length(points)
     router = ClusterTrees.LevelledTrees.Router(smallest_box_size, points[i])
-    root_state = root(tree), root_center, root_size, root_sfc_state
+    root_sector = 1
+    root_state = root(tree), root_sector, root_center, root_size, root_sfc_state
     ClusterTrees.update!(tree, root_state, i, router) do tree, node, data
         push!(ClusterTrees.data(tree,node).values, data)
     end
@@ -59,9 +61,13 @@ for b in ClusterTrees.LevelledTrees.LevelIterator(tree, length(tree.levels))
 end
 @assert num_points == length(points)
 
-# using Plots
-# plot()
-# for i in 1:length(bins)
-#     scatter!(bins[i])
-# end
-# plot!(legend=false)
+using Plots
+plot()
+for i in 1:length(bins)
+    x = getindex.(bins[i],1)
+    y = getindex.(bins[i],2)
+    z = getindex.(bins[i],3)
+    scatter!(x,y,z)
+end
+plot!(legend=false)
+gui()
